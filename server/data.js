c372ab37cc655b6163b1eba8c8ac6e66ce7518ee
Meier <meier.link24@gmail.com>
2025-01-28 22:06:36 +0000

    initial commit

---
+++ server/data.js
@@ -0,0 +1,31 @@
+
+//login info
+const usernameInput
+const passwordInput
+
+let usernameLogin
+let passwordLogin
+
+// func info
+
+const text
+const encryptionmethod
+const address //address of another user 
+const messageDeliveryStatus
+
+//server info
+
+const serverAddress
+const serverTime
+const serverMemory
+const serverUsers
+const serverRunningTime
+
+//const keyboard
+
+const keys [30] 
+const keysInput
+
+
+
+
